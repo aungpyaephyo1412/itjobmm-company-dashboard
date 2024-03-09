bd02f4eacedd9b91ef563051407e3538a8adabdf
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-03-09 12:22:21 +0630

    feat: progress :fire:

---
+++ app/home/layout.tsx
@@ -0,0 +1,23 @@
+"use client"
+import SideNavbar from "@/components/side-navbar";
+import {cn} from "@nextui-org/react";
+import {ReactNode} from "react";
+import Navigator from "@/components/navigator";
+import {usePathname} from "next/navigation";
+
+const Layout = ({children}:{children:ReactNode}) => {
+    const pathname = usePathname()
+    return (
+        <div
+            className={cn("min-h-screen w-full bg-white text-black flex ")}
+        >
+            <SideNavbar/>
+            <div className="p-8 w-full">
+                {pathname !== "/home" && <Navigator/>}
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
